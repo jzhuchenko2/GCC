ca99e1ed0286fc2891cd1ef0b4e3339d849739b8
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-11-26 21:19:53 -0600

    Update gcc.cpp

--- gcc.cpp
+++ gcc.cpp
@@ -62,3 +62,32 @@ int main() {
                     read-xml2.c    
 
 */
+
+/*
+int
+main(int argc, char **argv)
+{
+    xmlDoc         *document;
+    xmlNode        *root, *first_child, *node;
+    char           *filename;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s filename.xml\n", argv[0]);
+        return 1;
+    }
+    filename = argv[1];
+
+    document = xmlReadFile(filename, NULL, 0);
+    root = xmlDocGetRootElement(document);
+
+    understand this
+    stop skaking
+    fprintf(stdout, "Root is <%s> (%i)\n", root->name, root->type);
+    first_child = root->children;
+    for (node = first_child; node; node = node->next) {
+        fprintf(stdout, "\t Child is <%s> (%i)\n", node->name, node->type);
+    }
+    fprintf(stdout, "...\n");
+    return 0;
+}
+*/
